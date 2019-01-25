22427f468dc7fb62f8df32b83e92a18358588957
CarinaKr <carina.kraff@t-online.de>
2019-01-25 22:32:07 +0100

    interaction controller

---
+++ Assets/Scripts/InteractionController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionController : MonoBehaviour {
+
+    public float speed;
+    public string holdInteraction;
+    public string pickUp;
+    public Image[] holdCounter;
+
+    private Dictionary<InteractableObject, float> holdTime;
+    private bool isHolding;
+    private Rigidbody rb;
+    private List<InteractableObject> objects;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        holdTime = new Dictionary<InteractableObject, float>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if(Input.GetButton(holdInteraction))
+        {
+            if(!isHolding)
+            {
+                isHolding = true;
+                for(int i=0;i<objects.Count;i++)
+                {
+                    holdTime.Add(objects[i], objects[i].holdTime);
+                    holdCounter[i].enabled = true;
+                }
+                if(objects.Count==1)
+                {
+                    holdCounter[0].rectTransform.position = new Vector2(0, 0);
+                }
+                else if(objects.Count==2)
+                {
+                    holdCounter[0].rectTransform.position = new Vector2(0-holdCounter[0].minWidth / 2, 0);
+                    holdCounter[1].rectTransform.position = new Vector2(0+holdCounter[0].minWidth / 2, 0);
+                }
+            }
+            else
+            {
+                List<InteractableObject> obj = new List<InteractableObject>(holdTime.Keys);
+                for (int i= 0;i < obj.Count;i++)
+                {
+                    holdTime[obj[i]] = holdTime[obj[i]] - Time.deltaTime;
+                    holdCounter[i].fillAmount = obj[i].holdTime / holdTime[obj[i]];
+                    if(holdTime[obj[i]]<=0)
+                    {
+                        obj[i].HoldInteraction();
+                    }
+                }
+            }
+        }
+        else if(Input.GetButtonUp(holdInteraction))
+        {
+            isHolding = false;
+            holdTime.Clear();
+        }
+        else if(Input.GetButtonDown(pickUp))
+        {
+            foreach(InteractableObject obj in objects)
+            {
+                obj.PickUp();
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Object")
+        {
+            objects.Add(other.GetComponent<InteractableObject>());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag=="Object")
+        {
+            InteractableObject currentObject = other.GetComponent<InteractableObject>();
+            if (objects.Contains(currentObject))
+            {
+                objects.Remove(currentObject);
+            }
+            if(holdTime.ContainsKey(currentObject))
+            {
+                holdTime.Remove(currentObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/InteractableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+
+    public int holdTime;
+    
+    private bool state;
+
+    virtual public void HoldInteraction() { }
+    virtual public void PickUp() { }
+}
