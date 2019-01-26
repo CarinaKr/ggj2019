3565fe3cb49a99afd898fa86824984ef11bbcfbb
CarinaKr <carina.kraff@t-online.de>
2019-01-26 14:23:01 +0100

    candles points

---
+++ Assets/Scripts/Objects/Candle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : InteractableObject {
+
+    private GameObject candleLight;
+
+    override protected void Start()
+    {
+        base.Start();
+        candleLight = transform.GetChild(0).gameObject;
+        if (state)
+            candleLight.SetActive(true);
+        else if (!state)
+            candleLight.SetActive(false);
+    }
+
+    public override void HoldInteraction()
+    {
+        Debug.Log("Candle activated");
+        state = !state;
+        if (state)
+            candleLight.SetActive(true);
+        else if (!state)
+            candleLight.SetActive(false);
+    }
+    
+}
