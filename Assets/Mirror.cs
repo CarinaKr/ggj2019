0f2072165a2e783e7977e84bc399e82bda579881
VeritasFugit <veritasfugit@googlemail.com>
2019-01-26 20:14:02 +0100

    Mirror

---
+++ Assets/Mirror.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : InteractableObject {
+
+    public bool mirrorOpen = false;
+
+    public GameObject closedMirror;
+    public GameObject openMirror;
+
+    private void Start()
+    {
+        isMovable = false;
+    }
+
+    public override void HoldInteraction()
+    {
+        Debug.Log("open or close mirror");
+        state = !state;
+        if (state) {
+            mirrorOpen = true;
+            openMirror.SetActive(true);
+            openMirror.SetActive(false);
+        }                     
+        else
+        {
+            mirrorOpen = false;
+            openMirror.SetActive(false);
+            openMirror.SetActive(true);
+        }
+    }
+}
